20667139492ceebe9adbb8f10c34b6af2e033225
JulieBrusberg <julie@brusberg.dk>
2020-11-09 19:19:44 +0100

    n

--- Julie/js/java.js
+++ Julie/js/java.js
@@ -1,34 +1,48 @@
 console.log("Det virker");
 
-var videoSrcArr = Array("1","2","3","4","5","6","7","8","9","10","11","12","13","14","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"),
+var days = Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"),
     selectEl = document.getElementById('antal');
 
-for(var i = 0; i < videoSrcArr.length; i++){
-        selectEl.options.add( new Option(videoSrcArr[i], videoSrcArr[i]));
-    }       
+for (var i = 0; i < days.length; i++) {
+    selectEl.options.add(new Option(days[i], days[i]));
+}
 
-    var videoSrcArr = Array("Dage","Uger","Måneder"),
+var days = Array("Dage", "Uger", "Måneder"),
     selectEl = document.getElementById('langde');
 
-for(var i = 0; i < videoSrcArr.length; i++){
-        selectEl.options.add( new Option(videoSrcArr[i], videoSrcArr[i]));
-    }   
+for (var i = 0; i < days.length; i++) {
+    selectEl.options.add(new Option(days[i], days[i]));
+}
 
-    function beregn() {
+function beregn() {
 
-        var x = parseInt(document.getElementById("antal").value);
-        var y = parseInt(document.getElementById("tal").value);
-    
-        var result = x * 3 * y;
-        document.getElementById("result").value = result;
-    
-        var z = parseInt(document.getElementById("result").value);
-        var r = 20;
-    
-        var result = z / r;
-        document.getElementById("stammer").value = result
-    
-    }
+    var x = parseInt(document.getElementById("antal").value);
+    var y = parseInt(document.getElementById("tal").value);
+    var interval = parseInt(document.getElementById("langde").value);
+
+    console.log(interval);
+
+    var result;
+
+    if (interval == "Dage")
+        result = X * Y * 3;
+
+    else if (interval == "Uger")
+        result = X * Y * 3 * 7;
+
+    else if (interval == "Måneder")
+        result = X * Y * 3 * 30;
+
+
+    document.getElementById("result").value = result;
+
+    var z = parseInt(document.getElementById("result").value);
+    var r = 20;
+
+    var result = z / r;
+    document.getElementById("stammer").value = result
+
+}
 
 
 
