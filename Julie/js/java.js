76ea6ce7faddd38ba69a0d24e3f4e277bf01f5bd
JulieBrusberg <julie@brusberg.dk>
2020-11-09 20:40:40 +0100

    2

--- Julie/js/java.js
+++ Julie/js/java.js
@@ -1,3 +1,4 @@
+
 console.log("Det virker");
 
 var days = Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"),
@@ -8,7 +9,7 @@ for (var i = 0; i < days.length; i++) {
 }
 
 var days = Array("Dage", "Uger", "Måneder"),
-    selectEl = document.getElementById('langde');
+    selectEl = document.getElementById("days");
 
 for (var i = 0; i < days.length; i++) {
     selectEl.options.add(new Option(days[i], days[i]));
@@ -18,21 +19,21 @@ function beregn() {
 
     var x = parseInt(document.getElementById("antal").value);
     var y = parseInt(document.getElementById("tal").value);
-    var interval = parseInt(document.getElementById("langde").value);
+    var t = parseInt(document.getElementById("pris").value)
+    var interval = (document.getElementById("days").value);
 
     console.log(interval);
 
     var result;
 
     if (interval == "Dage")
-        result = X * Y * 3;
+        result = x * y * t / 20;
 
     else if (interval == "Uger")
-        result = X * Y * 3 * 7;
+        result = x * y * t / 20 * 7;
 
     else if (interval == "Måneder")
-        result = X * Y * 3 * 30;
-
+        result = x * y * t / 20 * 30;
 
     document.getElementById("result").value = result;
 
@@ -40,11 +41,5 @@ function beregn() {
     var r = 20;
 
     var result = z / r;
-    document.getElementById("stammer").value = result
-
+    document.getElementById("stammer").value = result;
 }
-
-
-
-
-
